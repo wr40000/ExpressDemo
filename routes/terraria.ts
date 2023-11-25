6ea573c75d9c728701c3da16b1a8ace0292ab081
wr <2119495210@qq.com>
2023-11-25 13:16:32 +0800

    add search onlineplayer And allplayer

--- routes/terraria.ts
+++ routes/terraria.ts
@@ -4,25 +4,61 @@ const fs = require("node:fs/promises");
 var router = express.Router();
 
 /* GET images page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   // 读取图片目录  server.log
   let imageDir1 = path.resolve(__dirname, '../../../../Terraria/TMLserver/tModLoader-Logs/server.log'); // 你的图片目录路径
-  console.log(imageDir1);
-  fs.readFile(imageDir1, 'utf8', (err, data)=>{
-    if(err){
-      console.error(err);
-      return;
-    }else{
-      console.log(data);      
-    }
-  })
 
-  try{
-    // const files = await fs.readdir(imageDir1);
-    // res.render('images', { title: 'My Images', images: files });
-  }catch(err){
-    console.log(err);
+  try {
+    const data = await fs.readFile(imageDir1, 'utf8');
+    // 正则表达式
+    const regexJoin = /: (.+?) has joined/g;
+    const regexLeave = /: (.+?) has left/g;
+
+    // 匹配结果存放的数组
+    const usernamesJoin = [];
+    const usernamesLeave = [];
+    let match;
+
+    // 使用正则表达式进行匹配
+    while ((match = regexLeave.exec(data)) !== null) {
+      usernamesLeave.push(match[1]);
+    }
+    while ((match = regexJoin.exec(data)) !== null) {
+      usernamesJoin.push(match[1]);
+    }
+    // 创建一个空对象来存储用户出现的次数
+    const countJoin = {};
+    const countLeave = {};
+    // 总用户
+    const countAllPlayer = new Set();
+    // 在线用户
+    const onlinePlayer = new Set()
+
+
+    // 遍历用户，计算在线用户
+    for (const str of usernamesLeave) {
+      // 如果对象中已经有该字符串作为键，就增加计数，否则初始化为1
+      countLeave[str] = (countLeave[str] || 0) + 1;
+    }
+    for (const str of usernamesJoin) {
+      countJoin[str] = (countJoin[str] || 0) + 1;
+    }
+    for (const str of usernamesJoin) {
+      countAllPlayer.add(str)
+      if(countJoin[str] > countLeave[str]){        
+        onlinePlayer.add(str)
+      }
+    }
+
+    // console.log(countAllPlayer, onlinePlayer);
+    
+    res.render('terraria', { terraria: { onlinePlayer, countAllPlayer } });
+  } catch (err) {
+    console.error(err);
+    // 在实际应用中，你可能希望返回一个错误页面或适当的错误响应。
+    res.status(500).send('Internal Server Error');
   }
 });
 
 module.exports = router;
+
